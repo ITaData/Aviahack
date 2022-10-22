74648c64a247bbda90d4c669d1f78a7c839e8fe0
Mikhail <mishagrishin03@gmail.com>
2022-10-22 15:54:49 +0300

    Создание базы данных

--- Space_military_bus/models.py
+++ Space_military_bus/models.py
@@ -17,13 +17,13 @@ class Driver(models.Model):
     def __str__(self):
         return self.id_driver
 
+
 class Operator(models.Model):
     operator_id = models.AutoField().primary_key()
     time_task_generation = models.TimeField()
     driver = models.ForeignKey(Driver, on_delete=models.PROTECT())
     def __str__(self):
         return self.operator_id
-
 
 class Trip (models.Model):
     trip_id = models.AutoField().primary_key()
