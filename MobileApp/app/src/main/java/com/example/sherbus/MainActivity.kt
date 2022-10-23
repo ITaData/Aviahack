5707551de92de5938c271842ac76800f928b973b
AtoshiSeito <ivgor02@gmail.com>
2022-10-23 11:08:04 +0300

    123213

--- MobileApp/app/src/main/java/com/example/sherbus/MainActivity.kt
+++ MobileApp/app/src/main/java/com/example/sherbus/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        var a: int = 3
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
